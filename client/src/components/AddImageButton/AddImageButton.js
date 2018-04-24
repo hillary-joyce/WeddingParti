b5e5d7ce6cd313da00d4ff5be0da7e1ff07db018
hillary-joyce <hillary.j.scofield@gmail.com>
2018-04-24 18:45:26 -0400

    created image gallery components

---
+++ client/src/components/AddImageButton/AddImageButton.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddImageButton = () => (
+  <button class="AddImg">Add Image</button>
+);
+
+export default AddImageButton;
---
+++ client/src/components/ImageGallery/ImageGallery.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ImageGallery = ({ children }) => (
+  <div className='imageGallery'>
+   {children}
+  </div>
+);
+
+export default ImageGallery;
