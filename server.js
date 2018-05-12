9980c69d41986e9d0881d8cfbf9971b49b064220
hillary-joyce <hillary.j.scofield@gmail.com>
2018-05-12 07:50:45 -0400

    yarn build

--- server.js
+++ server.js
@@ -13,13 +13,15 @@ app.use(express.static("client/build"));
 // Add routes, both API and view
 app.use(routes);
 
-// If deployed, use the deployed database. Otherwise use the local wedding database
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/weddingtest3";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
 
 
 
